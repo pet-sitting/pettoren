0454a81e356bfa79438c4befe1633e1fd015a450
Piotr Polanski <polanski.pio@gmail.com>
2021-07-18 14:54:43 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pets#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pets do
+    resources :bookings, only: [:create]
+  end
+  get "dashbord", to: 'dashboards#display'
+  patch "bookings/:id", to: 'bookings#update'
 end
