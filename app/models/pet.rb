8b40ead405290b5be20dd99c41372817193583a8
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-19 23:36:38 +0900

    add validates to model User Pet and Booking

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates :name, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true, length: { in: 20..1000 }
 end
