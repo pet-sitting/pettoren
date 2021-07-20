8f347c7085fd1e9ac4cc2a6d99879996f5a56fd2
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-21 00:22:45 +0900

    added 3 pet seeds with picture using cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-
+require "open-uri"
 
 User.destroy_all
 Pet.destroy_all
@@ -19,28 +19,41 @@ puts 'Creating 10 fake user...'
     name:    Faker::Name.name  ,
     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
     phone_number:  Faker::PhoneNumber.cell_phone_in_e164,
-    avatar_url: "https://res.cloudinary.com/ellie-xyb/image/upload/v1626789695/photo-1605001011156-cbf0b0f67a51_ywjezr.jpg"
+    avatar_url: ""
   )
   user.save!
 end
 
-puts "Creating 10 pets"
+puts "Creating 3 pets"
 
-Pet.create!(
+peta = Pet.new(
   user: User.all.sample,
-  name: "Spot",
-  species: "Golden Retriever",
-  picture_url: "https://res.cloudinary.com/ellie-xyb/image/upload/v1626789824/photo-1588022274210-7aab7c55c631_vwmy8x.jpg",
-  description: "Spot is very healthy, loves walking and sleeping in the sun."
-)
+  name: "Funky",
+  species: "Corgi",
+  description: "Funky is very funcy."
+  )
+file = URI.open('https://images.unsplash.com/photo-1557973557-ddfa9ee8c5bf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80')
+peta.pet_pic.attach(io: file, filename: 'funky.png', content_type: 'image/png')
+peta.save
 
-Pet.create!(
+petb = Pet.new(
   user: User.all.sample,
   name: "Zeke",
   species: "Border Collie",
-  picture_url: "https://res.cloudinary.com/ellie-xyb/image/upload/v1626790385/photo-1568393691622-c7ba131d63b4_v8kkxp.jpg",
   description: "Zeke is smart, doesn't like peeing on my pillow."
-)
+  )
+file = URI.open('https://images.unsplash.com/photo-1568393691622-c7ba131d63b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80')
+petb.pet_pic.attach(io: file, filename: 'funky.png', content_type: 'image/png')
+petb.save
 
+petc = Pet.new(
+  user: User.all.sample,
+  name: "Spot",
+  species: "Golden Retriever",
+  description: "Spot is very healthy, loves walking and sleeping in the sun."
+  )
+file = URI.open('https://images.unsplash.com/photo-1588022274210-7aab7c55c631?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+petc.pet_pic.attach(io: file, filename: 'spot.png', content_type: 'image/png')
+petc.save
 
 puts 'Finished!'
