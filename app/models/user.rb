826559e1a98f3d2de7ef4be78c60fc022cae1bbb
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-20 19:02:22 +0900

    change phone_number for user to be required true

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :phone_number, presence: true
   validates :address, presence: true
   has_many :pets, dependent: :destroy
   has_many :bookings_as_sitter, class_name: "Booking", foreign_key: :sitter_id
