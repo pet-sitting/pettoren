9af231497e91885c21ef1d9534cc60be299c1667
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-22 00:33:14 +0900

    change pic_url to use active_storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_142220) do
+ActiveRecord::Schema.define(version: 2021_07_21_152151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2021_07_21_142220) do
     t.string "name"
     t.string "species"
     t.bigint "user_id", null: false
-    t.string "picture_url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -71,7 +70,6 @@ ActiveRecord::Schema.define(version: 2021_07_21_142220) do
     t.string "name"
     t.string "address"
     t.string "phone_number"
-    t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
