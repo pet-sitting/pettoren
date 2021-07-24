92a39cf496dfa8fbd22520d8dba59ece9f9a3d20
Piotr Polanski <polanski.pio@gmail.com>
2021-07-24 14:11:22 +0900

    added working filters to index page

---
+++ app/javascript/components/filters.js
@@ -0,0 +1,20 @@
+const filterPets = () => {
+  const filters = document.querySelectorAll(".pet-filter");
+  const petCards = document.querySelectorAll(".pet-card");
+  filters.forEach((filter) => {
+    filter.addEventListener("click", (e) => {
+      console.log(e)
+      petCards.forEach((petCard) => {
+        petCard.hidden = true;
+        if (petCard.dataset.species === e.currentTarget.id) {
+          petCard.hidden = false;
+        } else if (e.currentTarget.id === "others" && !["dog", "cat", "bird"].includes(petCard.dataset.species)) {
+          petCard.hidden = false;
+        }
+      })
+    })
+  })
+}
+
+// filterPets();
+export { filterPets };
