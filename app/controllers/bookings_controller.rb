058405e20f7e7284c4a8e50b9f6e5d7094c376ff
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-24 15:16:07 +0900

    create booking and add boking show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,19 @@
 class BookingsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[confirm create]
-  skip_after_action :verify_authorized, only: %i[create confirm]
+  skip_before_action :authenticate_user!, only: %i[create show]
+  skip_after_action :verify_authorized, only: %i[create]
 
-  def confirm; end
+  def show
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
 
   def create
-    redirect_to bookings_confirm_path
+    params[:"booking-dates"].split(',').each do |date|
+      @booking = Booking.new(pet_id: params[:pet_id])
+      @booking.date = Date.parse(date)
+      @booking.sitter = current_user
+      @booking.save
+    end
+    redirect_to booking_path(@booking)
   end
 end
