faa8f0b3681a56d4786cb18e0119f121f206c66d
Piotr Polanski <polanski.pio@gmail.com>
2021-07-25 22:44:51 +0900

    testing on full db

--- db/seeds.rb
+++ db/seeds.rb
@@ -238,149 +238,156 @@ PetSchedule.create(date: '2021-8-18', pet: petd);
 PetSchedule.create(date: '2021-8-19', pet: petd);
 puts "Pet D created!"
 
-# pete = Pet.new(
-#   user: User.all.sample,
-#   name: "Kong",
-#   species: "cat",
-#   breed: "Persian",
-#   description: "Kong is grumpy and very fluffy."
-#   )
-# fileea = URI.open('https://images.unsplash.com/photo-1573148164257-8a2b173be464?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80')
-# pete.pet_pics.attach(io: fileea, filename: 'konga.png', content_type: 'image/png')
-# fileeb = URI.open('https://images.unsplash.com/photo-1614538899890-1a0d25d62e22?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80')
-# pete.pet_pics.attach(io: fileeb, filename: 'kongb.png', content_type: 'image/png')
-# fileec = URI.open('https://images.unsplash.com/photo-1620819436738-d830e5a46f63?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# pete.pet_pics.attach(io: fileec, filename: 'kongc.png', content_type: 'image/png')
-# fileed = URI.open('https://images.unsplash.com/photo-1577239559255-12a0a0fb92eb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# pete.pet_pics.attach(io: fileed, filename: 'kongd.png', content_type: 'image/png')
-# pete.save
-# PetSchedule.create(date: '2021-7-24', pet: pete);
-# PetSchedule.create(date: '2021-7-25', pet: pete);
-# PetSchedule.create(date: '2021-7-29', pet: pete);
-# PetSchedule.create(date: '2021-7-30', pet: pete);
-# PetSchedule.create(date: '2021-7-31', pet: pete);
-# PetSchedule.create(date: '2021-8-3', pet: pete);
-# PetSchedule.create(date: '2021-8-18', pet: pete);
-# PetSchedule.create(date: '2021-8-19', pet: pete);
+pete = Pet.new(
+  user: User.all.sample,
+  name: "Kong",
+  species: "cat",
+  breed: "Persian",
+  description: "Kong is grumpy and very fluffy."
+  )
+fileea = URI.open('https://images.unsplash.com/photo-1573148164257-8a2b173be464?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80')
+pete.pet_pics.attach(io: fileea, filename: 'konga.png', content_type: 'image/png')
+fileeb = URI.open('https://images.unsplash.com/photo-1614538899890-1a0d25d62e22?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80')
+pete.pet_pics.attach(io: fileeb, filename: 'kongb.png', content_type: 'image/png')
+fileec = URI.open('https://images.unsplash.com/photo-1620819436738-d830e5a46f63?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+pete.pet_pics.attach(io: fileec, filename: 'kongc.png', content_type: 'image/png')
+fileed = URI.open('https://images.unsplash.com/photo-1577239559255-12a0a0fb92eb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+pete.pet_pics.attach(io: fileed, filename: 'kongd.png', content_type: 'image/png')
+pete.save
+PetSchedule.create(date: '2021-7-24', pet: pete);
+PetSchedule.create(date: '2021-7-25', pet: pete);
+PetSchedule.create(date: '2021-7-29', pet: pete);
+PetSchedule.create(date: '2021-7-30', pet: pete);
+PetSchedule.create(date: '2021-7-31', pet: pete);
+PetSchedule.create(date: '2021-8-3', pet: pete);
+PetSchedule.create(date: '2021-8-18', pet: pete);
+PetSchedule.create(date: '2021-8-19', pet: pete);
+puts "Pet E created!"
 
-# petf = Pet.new(
-#   user: User.all.sample,
-#   name: "Rufus",
-#   species: "cat",
-#   breed: "Sphynx",
-#   description: "Lost his hair in an unfortunate accident."
-#   )
-# filefa = URI.open('https://images.unsplash.com/photo-1520315342629-6ea920342047?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80')
-# petf.pet_pics.attach(io: filefa, filename: 'rufusa.png', content_type: 'image/png')
-# filefb = URI.open('https://images.unsplash.com/photo-1607623488981-3b72c361204f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# petf.pet_pics.attach(io: filefb, filename: 'rufusb.png', content_type: 'image/png')
-# filefc = URI.open('https://images.unsplash.com/photo-1530391277665-49655f7b0b90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# petf.pet_pics.attach(io: filefc, filename: 'rufusc.png', content_type: 'image/png')
-# petf.save
-# PetSchedule.create(date: '2021-7-24', pet: petf);
-# PetSchedule.create(date: '2021-7-28', pet: petf);
-# PetSchedule.create(date: '2021-7-29', pet: petf);
-# PetSchedule.create(date: '2021-8-1', pet: petf);
-# PetSchedule.create(date: '2021-8-2', pet: petf);
-# PetSchedule.create(date: '2021-8-3', pet: petf);
-# PetSchedule.create(date: '2021-8-15', pet: petf);
-# PetSchedule.create(date: '2021-8-16', pet: petf);
+petf = Pet.new(
+  user: User.all.sample,
+  name: "Rufus",
+  species: "cat",
+  breed: "Sphynx",
+  description: "Lost his hair in an unfortunate accident."
+  )
+filefa = URI.open('https://images.unsplash.com/photo-1520315342629-6ea920342047?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80')
+petf.pet_pics.attach(io: filefa, filename: 'rufusa.png', content_type: 'image/png')
+filefb = URI.open('https://images.unsplash.com/photo-1607623488981-3b72c361204f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+petf.pet_pics.attach(io: filefb, filename: 'rufusb.png', content_type: 'image/png')
+filefc = URI.open('https://images.unsplash.com/photo-1530391277665-49655f7b0b90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+petf.pet_pics.attach(io: filefc, filename: 'rufusc.png', content_type: 'image/png')
+petf.save
+PetSchedule.create(date: '2021-7-24', pet: petf);
+PetSchedule.create(date: '2021-7-28', pet: petf);
+PetSchedule.create(date: '2021-7-29', pet: petf);
+PetSchedule.create(date: '2021-8-1', pet: petf);
+PetSchedule.create(date: '2021-8-2', pet: petf);
+PetSchedule.create(date: '2021-8-3', pet: petf);
+PetSchedule.create(date: '2021-8-15', pet: petf);
+PetSchedule.create(date: '2021-8-16', pet: petf);
+puts "Pet F created!"
 
-# petg = Pet.new(
-#   user: User.all.sample,
-#   name: "Killer",
-#   species: "dog",
-#   breed: "Yorkshire Terrier",
-#   description: "Not as dangerous as he sounds."
-#   )
-# filega = URI.open('https://images.unsplash.com/photo-1551290464-66719418ca54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1355&q=80')
-# petg.pet_pics.attach(io: filega, filename: 'killera.png', content_type: 'image/png')
-# filegb = URI.open('https://images.unsplash.com/photo-1547482354-89d4259dbc4b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# petg.pet_pics.attach(io: filegb, filename: 'killerb.png', content_type: 'image/png')
-# filegc = URI.open('https://images.unsplash.com/photo-1618760877592-0d80e8b7bd02?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# petg.pet_pics.attach(io: filegc, filename: 'killerc.png', content_type: 'image/png')
-# filegd = URI.open('https://images.unsplash.com/photo-1443806798002-651c462956ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1352&q=80')
-# petg.pet_pics.attach(io: filegd, filename: 'killerd.png', content_type: 'image/png')
-# petg.save
-# PetSchedule.create(date: '2021-7-25', pet: petg);
-# PetSchedule.create(date: '2021-7-27', pet: petg);
-# PetSchedule.create(date: '2021-7-29', pet: petg);
-# PetSchedule.create(date: '2021-8-6', pet: petg);
-# PetSchedule.create(date: '2021-8-7', pet: petg);
-# PetSchedule.create(date: '2021-8-15', pet: petg);
-# PetSchedule.create(date: '2021-8-18', pet: petg);
-# PetSchedule.create(date: '2021-8-19', pet: petg);
+petg = Pet.new(
+  user: User.all.sample,
+  name: "Killer",
+  species: "dog",
+  breed: "Yorkshire Terrier",
+  description: "Not as dangerous as he sounds."
+  )
+filega = URI.open('https://images.unsplash.com/photo-1551290464-66719418ca54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1355&q=80')
+petg.pet_pics.attach(io: filega, filename: 'killera.png', content_type: 'image/png')
+filegb = URI.open('https://images.unsplash.com/photo-1547482354-89d4259dbc4b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+petg.pet_pics.attach(io: filegb, filename: 'killerb.png', content_type: 'image/png')
+filegc = URI.open('https://images.unsplash.com/photo-1618760877592-0d80e8b7bd02?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+petg.pet_pics.attach(io: filegc, filename: 'killerc.png', content_type: 'image/png')
+filegd = URI.open('https://images.unsplash.com/photo-1443806798002-651c462956ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1352&q=80')
+petg.pet_pics.attach(io: filegd, filename: 'killerd.png', content_type: 'image/png')
+petg.save
+PetSchedule.create(date: '2021-7-25', pet: petg);
+PetSchedule.create(date: '2021-7-27', pet: petg);
+PetSchedule.create(date: '2021-7-29', pet: petg);
+PetSchedule.create(date: '2021-8-6', pet: petg);
+PetSchedule.create(date: '2021-8-7', pet: petg);
+PetSchedule.create(date: '2021-8-15', pet: petg);
+PetSchedule.create(date: '2021-8-18', pet: petg);
+PetSchedule.create(date: '2021-8-19', pet: petg);
+puts "Pet G created!"
 
-# peth = Pet.new(
-#   user: User.all.sample,
-#   name: "ChiChi",
-#   species: "dog",
-#   breed: "Chihuahua",
-#   description: "Very loud sometimes, but gentle."
-#   )
-# fileha = URI.open('https://images.unsplash.com/photo-1566793849773-e1375992cd59?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# peth.pet_pics.attach(io: fileha, filename: 'chichia.png', content_type: 'image/png')
-# filehb = URI.open('https://images.unsplash.com/photo-1598283027164-ce17967a47d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80')
-# peth.pet_pics.attach(io: filehb, filename: 'chichib.png', content_type: 'image/png')
-# filehc = URI.open('https://images.unsplash.com/photo-1564510015443-605afdb7dcbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# peth.pet_pics.attach(io: filehc, filename: 'chichic.png', content_type: 'image/png')
-# filehd = URI.open('https://images.unsplash.com/photo-1559681369-e8b09c685cf2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80')
-# peth.pet_pics.attach(io: filehd, filename: 'chichid.png', content_type: 'image/png')
-# peth.save
-# PetSchedule.create(date: '2021-7-27', pet: peth);
-# PetSchedule.create(date: '2021-7-28', pet: peth);
-# PetSchedule.create(date: '2021-7-29', pet: peth);
-# PetSchedule.create(date: '2021-8-1', pet: peth);
-# PetSchedule.create(date: '2021-8-2', pet: peth);
-# PetSchedule.create(date: '2021-8-15', pet: peth);
-# PetSchedule.create(date: '2021-8-18', pet: peth);
-# PetSchedule.create(date: '2021-8-19', pet: peth);
+peth = Pet.new(
+  user: User.all.sample,
+  name: "ChiChi",
+  species: "dog",
+  breed: "Chihuahua",
+  description: "Very loud sometimes, but gentle."
+  )
+fileha = URI.open('https://images.unsplash.com/photo-1566793849773-e1375992cd59?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+peth.pet_pics.attach(io: fileha, filename: 'chichia.png', content_type: 'image/png')
+filehb = URI.open('https://images.unsplash.com/photo-1598283027164-ce17967a47d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80')
+peth.pet_pics.attach(io: filehb, filename: 'chichib.png', content_type: 'image/png')
+filehc = URI.open('https://images.unsplash.com/photo-1564510015443-605afdb7dcbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+peth.pet_pics.attach(io: filehc, filename: 'chichic.png', content_type: 'image/png')
+filehd = URI.open('https://images.unsplash.com/photo-1559681369-e8b09c685cf2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80')
+peth.pet_pics.attach(io: filehd, filename: 'chichid.png', content_type: 'image/png')
+peth.save
+PetSchedule.create(date: '2021-7-27', pet: peth);
+PetSchedule.create(date: '2021-7-28', pet: peth);
+PetSchedule.create(date: '2021-7-29', pet: peth);
+PetSchedule.create(date: '2021-8-1', pet: peth);
+PetSchedule.create(date: '2021-8-2', pet: peth);
+PetSchedule.create(date: '2021-8-15', pet: peth);
+PetSchedule.create(date: '2021-8-18', pet: peth);
+PetSchedule.create(date: '2021-8-19', pet: peth);
+puts "Pet H created!"
 
-# peti = Pet.new(
-#   user: User.all.sample,
-#   name: "Bugs",
-#   species: "rabbit",
-#   breed: "American",
-#   description: "Timid, likes carrots and lettuce."
-#   )
-# fileia = URI.open('https://images.unsplash.com/photo-1556838803-cc94986cb631?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80')
-# peti.pet_pics.attach(io: fileia, filename: 'bugsa.png', content_type: 'image/png')
-# fileib = URI.open('https://images.unsplash.com/photo-1469532804526-98aa275b657c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80')
-# peti.pet_pics.attach(io: fileib, filename: 'bugsb.png', content_type: 'image/png')
-# fileic = URI.open('https://images.unsplash.com/photo-1585969646097-a1b0038c37a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# peti.pet_pics.attach(io: fileic, filename: 'bugsc.png', content_type: 'image/png')
-# peti.save
-# PetSchedule.create(date: '2021-7-28', pet: peti);
-# PetSchedule.create(date: '2021-7-29', pet: peti);
-# PetSchedule.create(date: '2021-7-30', pet: peti);
-# PetSchedule.create(date: '2021-8-2', pet: peti);
-# PetSchedule.create(date: '2021-8-3', pet: peti);
-# PetSchedule.create(date: '2021-8-4', pet: peti);
-# PetSchedule.create(date: '2021-8-5', pet: peti);
-# PetSchedule.create(date: '2021-8-6', pet: peti);
+peti = Pet.new(
+  user: User.all.sample,
+  name: "Bugs",
+  species: "rabbit",
+  breed: "American",
+  description: "Timid, likes carrots and lettuce."
+  )
+fileia = URI.open('https://images.unsplash.com/photo-1556838803-cc94986cb631?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80')
+peti.pet_pics.attach(io: fileia, filename: 'bugsa.png', content_type: 'image/png')
+fileib = URI.open('https://images.unsplash.com/photo-1469532804526-98aa275b657c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80')
+peti.pet_pics.attach(io: fileib, filename: 'bugsb.png', content_type: 'image/png')
+fileic = URI.open('https://images.unsplash.com/photo-1585969646097-a1b0038c37a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+peti.pet_pics.attach(io: fileic, filename: 'bugsc.png', content_type: 'image/png')
+peti.save
+PetSchedule.create(date: '2021-7-28', pet: peti);
+PetSchedule.create(date: '2021-7-29', pet: peti);
+PetSchedule.create(date: '2021-7-30', pet: peti);
+PetSchedule.create(date: '2021-8-2', pet: peti);
+PetSchedule.create(date: '2021-8-3', pet: peti);
+PetSchedule.create(date: '2021-8-4', pet: peti);
+PetSchedule.create(date: '2021-8-5', pet: peti);
+PetSchedule.create(date: '2021-8-6', pet: peti);
+puts "Pet I created!"
 
-# petj = Pet.new(
-#   user: User.all.sample,
-#   name: "Eddie",
-#   species: "dog",
-#   breed: "Jack Russell",
-#   description: "Famous for his role in Frasier."
-#   )
-# fileja = URI.open('https://images.unsplash.com/photo-1613029226236-b3c704806aa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# petj.pet_pics.attach(io: fileja, filename: 'eddiea.png', content_type: 'image/png')
-# filejb = URI.open('https://images.unsplash.com/photo-1571570307831-0fe3b120bafe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# petj.pet_pics.attach(io: filejb, filename: 'eddieb.png', content_type: 'image/png')
-# filejc = URI.open('https://images.unsplash.com/photo-1613029249698-48cfdd88d47b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# petj.pet_pics.attach(io: filejc, filename: 'eddiec.png', content_type: 'image/png')
-# filejd = URI.open('https://images.unsplash.com/photo-1554039533-eb4a99deaf72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
-# petj.pet_pics.attach(io: filejd, filename: 'eddied.png', content_type: 'image/png')
-# petj.save
-# PetSchedule.create(date: '2021-7-24', pet: petj);
-# PetSchedule.create(date: '2021-7-25', pet: petj);
-# PetSchedule.create(date: '2021-7-26', pet: petj);
-# PetSchedule.create(date: '2021-7-29', pet: petj);
-# PetSchedule.create(date: '2021-7-30', pet: petj);
-# PetSchedule.create(date: '2021-7-31', pet: petj);
-# PetSchedule.create(date: '2021-8-3', pet: petj);
-# PetSchedule.create(date: '2021-8-4', pet: petj);
+petj = Pet.new(
+  user: User.all.sample,
+  name: "Eddie",
+  species: "dog",
+  breed: "Jack Russell",
+  description: "Famous for his role in Frasier."
+  )
+fileja = URI.open('https://images.unsplash.com/photo-1613029226236-b3c704806aa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+petj.pet_pics.attach(io: fileja, filename: 'eddiea.png', content_type: 'image/png')
+filejb = URI.open('https://images.unsplash.com/photo-1571570307831-0fe3b120bafe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+petj.pet_pics.attach(io: filejb, filename: 'eddieb.png', content_type: 'image/png')
+filejc = URI.open('https://images.unsplash.com/photo-1613029249698-48cfdd88d47b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+petj.pet_pics.attach(io: filejc, filename: 'eddiec.png', content_type: 'image/png')
+filejd = URI.open('https://images.unsplash.com/photo-1554039533-eb4a99deaf72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
+petj.pet_pics.attach(io: filejd, filename: 'eddied.png', content_type: 'image/png')
+petj.save
+PetSchedule.create(date: '2021-7-24', pet: petj);
+PetSchedule.create(date: '2021-7-25', pet: petj);
+PetSchedule.create(date: '2021-7-26', pet: petj);
+PetSchedule.create(date: '2021-7-29', pet: petj);
+PetSchedule.create(date: '2021-7-30', pet: petj);
+PetSchedule.create(date: '2021-7-31', pet: petj);
+PetSchedule.create(date: '2021-8-3', pet: petj);
+PetSchedule.create(date: '2021-8-4', pet: petj);
+puts "Pet J created!"
+
 puts 'Finished!'
