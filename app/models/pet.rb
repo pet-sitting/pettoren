4eb3f1be0189ef720262ea50d6f918c82c9ae181
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-27 00:21:07 +0900

    update user menu

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,6 +4,8 @@ class Pet < ApplicationRecord
   has_many :bookings
   has_many :pet_schedules, dependent: :destroy
   validates :name, presence: true
+  validates :breed, presence: true
+  validates :species, presence: true
   validates :user_id, presence: true
   validates :description, presence: true, length: { in: 20..1000 }
 end
