9c654532daf04a1cb3b1a477ccced475c49c858d
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-29 02:49:22 +0900

    add routes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,21 @@
 class DashboardsController < ApplicationController
+  before_action :authorize_current_user, only: %i[display pets bookings requests]
+
   def display
+  end
+
+  def pets
+  end
+
+  def bookings
+  end
+
+  def requests
+  end
+
+  private
+
+  def authorize_current_user
     authorize :dashboard, :display?
     @user = current_user
   end
