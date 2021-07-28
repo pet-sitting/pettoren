2f2bcb7a8b9ec26778533395117bac72ebf3a631
Piotr Polanski <polanski.pio@gmail.com>
2021-07-28 19:24:19 +0900

    fixed no map, no carousel bug

--- app/javascript/components/counter.js
+++ app/javascript/components/counter.js
@@ -5,25 +5,27 @@ const counter = () => {
   const petCount = document.getElementById("pet-count");
   const picCount = document.getElementById("pic-count");
 
-  const countUser = new CountUp('user-count', userCount.dataset.count);
-  if (!countUser.error) {
-    countUser.start();
-  } else {
-    console.error(countUser.error);
-  }
+  if (userCount !== null && petCount !== null && picCount !== null) {
+    const countUser = new CountUp('user-count', userCount.dataset.count);
+    if (!countUser.error) {
+      countUser.start();
+    } else {
+      console.error(countUser.error);
+    }
 
-  const countPet = new CountUp('pet-count', petCount.dataset.count);
-  if (!countPet.error) {
-    countPet.start();
-  } else {
-    console.error(countPet.error);
-  }
+    const countPet = new CountUp('pet-count', petCount.dataset.count);
+    if (!countPet.error) {
+      countPet.start();
+    } else {
+      console.error(countPet.error);
+    }
 
-  const countPic = new CountUp('pic-count', picCount.dataset.count);
-  if (!countPic.error) {
-    countPic.start();
-  } else {
-    console.error(countPic.error);
+    const countPic = new CountUp('pic-count', picCount.dataset.count);
+    if (!countPic.error) {
+      countPic.start();
+    } else {
+      console.error(countPic.error);
+    }
   }
 }
 
