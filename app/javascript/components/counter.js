e0a90cebd5e439f0a4ce37dc8c6a94ff9bf1de4e
Piotr Polanski <polanski.pio@gmail.com>
2021-07-28 23:30:17 +0900

    animation on scroll working

--- app/javascript/components/counter.js
+++ app/javascript/components/counter.js
@@ -5,33 +5,35 @@ const counter = () => {
   const petCount = document.getElementById("pet-count");
   const picCount = document.getElementById("pic-count");
 
-  // window.onscroll = function () {
-  //   let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  //   if (scrolled >= 750) {
-  //     console.log("test1");
-  //   }
-  // }
+  window.onscroll = myScroll;
+  let counter = 0;
+  function myScroll() {
+    if (counter == 0) {
+      if (window.pageYOffset > 750) {
+        if (userCount !== null && petCount !== null && picCount !== null) {
+          const countUser = new CountUp('user-count', userCount.dataset.count);
+          if (!countUser.error) {
+            countUser.start();
+          } else {
+            console.error(countUser.error);
+          }
 
-  if (userCount !== null && petCount !== null && picCount !== null) {
-    const countUser = new CountUp('user-count', userCount.dataset.count);
-    if (!countUser.error) {
-      countUser.start();
-    } else {
-      console.error(countUser.error);
-    }
+          const countPet = new CountUp('pet-count', petCount.dataset.count);
+          if (!countPet.error) {
+            countPet.start();
+          } else {
+            console.error(countPet.error);
+          }
 
-    const countPet = new CountUp('pet-count', petCount.dataset.count);
-    if (!countPet.error) {
-      countPet.start();
-    } else {
-      console.error(countPet.error);
-    }
-
-    const countPic = new CountUp('pic-count', picCount.dataset.count);
-    if (!countPic.error) {
-      countPic.start();
-    } else {
-      console.error(countPic.error);
+          const countPic = new CountUp('pic-count', picCount.dataset.count);
+          if (!countPic.error) {
+            countPic.start();
+          } else {
+            console.error(countPic.error);
+          }
+        }
+        counter++;
+      }
     }
   }
 }
