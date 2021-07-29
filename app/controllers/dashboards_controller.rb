6568e8c19fdb468a29079542f65fdd1306dbe895
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-29 21:44:48 +0900

    booking requests view

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,10 @@
 class DashboardsController < ApplicationController
-  before_action :authorize_current_user, only: %i[display pets bookings requests]
+  before_action :authorize_current_user, only: %i[display]
 
   def display
     @pets = @user.pets
     @bookings = @user.bookings_as_sitter
+    @requests = current_user.bookings_as_owner
   end
 
   private
