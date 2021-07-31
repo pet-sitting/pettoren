76fbefd6e16e2fab40527e5e023e850a0d328a8f
Piotr Polanski <polanski.pio@gmail.com>
2021-07-31 13:08:51 +0900

    filters works

--- app/javascript/components/filters.js
+++ app/javascript/components/filters.js
@@ -1,14 +1,25 @@
 const filterPets = () => {
   const filters = document.querySelectorAll(".pet-filter");
   const petCards = document.querySelectorAll(".pet-card");
+  const headerPetsAll = document.getElementById("filter")
+
+  // const capitalize = (string) => {
+  //   string[0].toUpperCase() + string.substring(1)
+  // }
+
   filters.forEach((filter) => {
     filter.addEventListener("click", (e) => {
       petCards.forEach((petCard) => {
         petCard.hidden = true;
         if (petCard.dataset.species === e.currentTarget.id) {
+          headerPetsAll.innerText = e.currentTarget.id[0].toUpperCase() + e.currentTarget.id.substring(1)
           petCard.hidden = false;
-        } else if (e.currentTarget.id === "others" && !["dog", "cat", "bird"].includes(petCard.dataset.species)) {
+        } else if (e.currentTarget.id === "others" && !["dog", "cat", "rabbit"].includes(petCard.dataset.species)) {
           petCard.hidden = false;
+          headerPetsAll.innerText = "Others"
+        } else if (e.currentTarget.id === "all") {
+          petCard.hidden = false;
+          headerPetsAll.innerText = "All pets"
         }
       })
     })
